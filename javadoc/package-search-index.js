9675fc0180084b4fe65ca4cb92ded1f9f39e4e13
JohanVonElectrum <joanestradium@gmail.com>
2024-02-27 08:17:17 +0000

    Deploying to javadoc from @ Koralix-Studios/koralix-commons@627b26b320ce4e8272b912fd5d0612144df8a637 🚀

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.koralix.commons.minecraft.serialization"},{"l":"com.koralix.commons.serialization"},{"l":"com.koralix.commons.value"}];updateSearchResults();
